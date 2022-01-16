534fc6c74de55ae45e4ca54ed84512f3123eca7a
Elsayed Elbeshry <elsayedelbeshry119@gmail.com>
2022-01-16 21:11:53 +0200

    Admin dashboard

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
+
+// Route::prefix('admin')->group(function () {
+//     Route::get('test', function () {
+//         return 'test';
+//     });
+// });
